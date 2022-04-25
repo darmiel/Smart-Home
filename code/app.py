c9053d727e607f8765c2fec5237ad0813ba5bbe2
Luca Hackl <lucamiguelhackl@gmail.com>
2022-04-25 21:55:27 +0200

    Refactor (last time)

--- code/app.py
+++ code/app.py
@@ -1,5 +1,12 @@
+import eventlet
+eventlet.monkey_patch()
+
 from flask import Flask
 from flask_socketio import SocketIO
+
+app = Flask(__name__)
+
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 from dotenv import load_dotenv
 import os
@@ -10,16 +17,6 @@ from wine import wine as wine_blueprint
 
 from login import login_manager
 from models import db
-
-import eventlet
-
-eventlet.monkey_patch()
-
-
-app = Flask(__name__)
-
-socketio = SocketIO(app, cors_allowed_origins='*')
-
 
 load_dotenv()
 app.secret_key = os.getenv('secret_key')
@@ -35,12 +32,11 @@ app.register_blueprint(cw_blueprint)
 app.register_blueprint(login_blueprint)
 app.register_blueprint(wine_blueprint)
 
-
 @app.before_first_request
 def create_all():
     db.create_all()
 
 
 if __name__ == "__main__":
-    #   socketio.run(app, host='0.0.0.0', port=8181, keyfile='key.pem', certfile='cert.pem')
-    socketio.run(app, host='0.0.0.0', port=8181)
+#   socketio.run(app, host='0.0.0.0', port=8181, keyfile='key.pem', certfile='cert.pem')
+   socketio.run(app, host='0.0.0.0',port=8181)
